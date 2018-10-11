0e9c405937c3404a1ae35acc3edf71a4c966fb41
Chance Phiniezy <phiniezyc@gmail.com>
2018-10-10 20:32:07 -0400

    Adds commonent explaining props in class vs. functional component

--- tacky/src/Components/SampleFunctionalComponentFile.js
+++ tacky/src/Components/SampleFunctionalComponentFile.js
@@ -8,6 +8,8 @@ function SampleFunctionalComponentFile(props) {
         <div>
         <h2>Yo Fahd</h2>
         <p>This is a functional Component, structure it like this if your component doesn't have a need for state.  If it just returns something</p>
+
+        {/* In a class component it is the same but must do this.props... */}
         <h3>This is the passed prop from the main app component: {props.sampleProp}</h3>
         </div>
     )
